1027ca822555b4d1ce72616c0c3fbf338172fdfd
engkos <engkoskostaman759@gmail.com>
2022-10-16 18:02:04 +0700

    update

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const API = axios.create(
   {
-    baseURL: "https://kendaraan-apk.herokuapp.com/api/v1/",
+    baseURL: "https://dbappkendaraan.herokuapp.com/api/v1/",
   }
   //   {
   //   baseURL: process.env.REACT_APP_BASEURL,
