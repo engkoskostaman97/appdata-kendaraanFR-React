d6ce4039f1f15bee7a44026123ea35d9b123adc8
engkos kostaman <engkos.kostaman@bni.co.id>
2023-09-19 10:19:25 +0700

    update

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const API = axios.create(
   {
-    baseURL: "https://dbappkendaraan.herokuapp.com/api/v1/",
+    baseURL: "http://localhost:8080/api/v1",
   }
   //   {
   //   baseURL: process.env.REACT_APP_BASEURL,
